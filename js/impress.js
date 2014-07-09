4ea50ea0a446d3af503feb4923f7eead041dfe1b
CarloBellettini <carlo.bellettini@unimi.it>
2014-07-09 12:19:48 +0200

    Esc Key :  go to the step with “overview” id  (thx to https://github.com/bkummel/impress.js )

--- js/impress.js
+++ js/impress.js
@@ -827,7 +827,8 @@
         //   as another way to moving to next step... And yes, I know that for the sake of
         //   consistency I should add [shift+tab] as opposite action...
         document.addEventListener("keyup", function ( event ) {
-            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40) ) {
+            if ( event.keyCode === 9 || ( event.keyCode >= 32 && event.keyCode <= 34 ) || (event.keyCode >= 37 && event.keyCode <= 40)
+		|| event.keyCode === 27) {
                 switch( event.keyCode ) {
                     case 33: // pg up
                     case 37: // left
@@ -841,6 +842,11 @@
                     case 40: // down
                              api.next();
                              break;
+                    case 27: // esc
+                             if ( api.goto("overview") ) {
+                                event.stopImmediatePropagation();
+                                event.preventDefault();
+}                    
                 }
                 
                 event.preventDefault();
